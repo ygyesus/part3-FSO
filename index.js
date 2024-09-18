e25f16aaedce1eeae38936dc157f15e9ff83fe59
ygyesus <yosefgyesus.in@gmail.com>
2024-09-18 15:36:59 +0300

    Add process.env.port

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.delete('/api/persons/:id', (req, res) => {
     phonebook = phonebook.filter(person => person.id !== id)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
